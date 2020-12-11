e40ce3cf82fc4c709ff40ca89ff215e1611fa68d
herveGuigoz <herve.guigoz@gmail.com>
2020-12-11 11:03:40 +0100

    ✨ feat: handle invalid url and path errors

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,17 @@ Future<void> main(String url, String path) async {
 
   /// get Path model.
   final pathModel = container.read(pathProvider(path));
+  if (pathModel == null) {
+    stderr.writeln('error: undefine $path in specifications');
+    exit(2);
+  }
 
   /// get definition reference for GET method.
-  final ref = pathModel.getRessource.responses.first.ref;
+  final ref = pathModel?.getRessource?.responses?.first?.ref;
+  if (ref == null) {
+    stderr.writeln('error: no GET method found in specifications');
+    exit(2);
+  }
 
   /// get Definition model.
   final def = container.read(definitionProvider(ref));
