d5357a62e8cba9e72aeb7ae9092512a827da830a
herveGuigoz <herve.guigoz@gmail.com>
2020-12-11 19:38:23 +0100

    ✨ feat: add result helper to handle errors

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:riverpod/all.dart';
 
-import 'models/path.dart';
 import 'output/file_manager.dart';
 import 'providers.dart';
 
@@ -16,61 +15,41 @@ Future<void> main(String url, String path, String method) async {
   );
 
   /// load open api documentation.
-  await container.read(specsProvider.future).catchError(
-    (err) {
-      stderr.writeln('error: $url is invalid');
-      exit(2);
-    },
-  );
+  await container.read(specsProvider.future).catchError((_) {
+    stderr.writeln('error: $url is invalid');
+    exit(2);
+  });
 
   /// get Path model.
-  try {
-    container.read(pathModelProvider);
-  } catch (err) {
-    stderr.writeln('error: $err');
+  container.read(pathModelProvider).onError((err) {
+    stderr.writeln('$err');
     exit(2);
-  }
+  });
 
-  /// get definition reference for method (GET, PUT, POST).
-  String ref;
-  try {
-    final responseModel = container.read(getResponseForPathAndMethod);
-    ref = responseModel.ref;
-  } catch (err) {
-    stderr.writeln('error: $err');
+  /// get definition reference for method (GET, POST, PUT).
+  final result = container.read(getResponseForPathAndMethod);
+  result.onError((err) {
+    stderr.writeln('$err');
     exit(2);
-  }
-  if (ref == null) {
-    stderr.writeln('error: No definition found in specification');
-    exit(2);
-  }
+  });
 
   /// get Definition model.
-  final def = container.read(definitionProvider(ref));
+  final def = container.read(definitionProvider(result.dataOrThrow.value));
+  if (def == null) {
+    stderr.writeln('Definition not found');
+    exit(2);
+  }
 
   /// write file(s).
-  await container.read(writeToFile(def).future);
+  await container.read(writeToFile(def).future).catchError((err) {
+    stderr.writeln('$err');
+    exit(2);
+  });
 
   /// create main file with parts of.
   final stringBuffer = container.read(stringBufferProvider);
   final fileName = container.read(mainFileNameProvider);
   await FileManager(container.read, stringBuffer: stringBuffer).save(fileName);
 
-  return;
+  exit(0);
 }
-
-// String _getDefinitionForGivenPathAndMethod(Path path) {
-//   String ref;
-//   try {
-//     ref = path.getRessourceByMethod('get')?.responses?.first?.ref;
-//   } catch (err) {
-//     stderr.writeln('error: $err');
-//     exit(2);
-//   }
-//   if (ref == null) {
-//     stderr.writeln('error: No definition found in specification');
-//     exit(2);
-//   }
-
-//   return ref;
-// }
---
+++ lib/models/result.dart
@@ -0,0 +1,35 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'result.freezed.dart';
+
+@freezed
+abstract class Result<T> with _$Result<T> {
+  const Result._();
+  const factory Result.data(@nullable T value) = _ResultData<T>;
+  const factory Result.error(dynamic error) = _ResultError<T>;
+
+  // Retrun either data or error from Future.
+  static Future<Result<T>> guardFuture<T>(Future<T> Function() future) async {
+    try {
+      return Result.data(await future());
+    } catch (error) {
+      return Result.error(error);
+    }
+  }
+
+  T get dataOrThrow {
+    return when(
+      data: (value) => value,
+      error: (err) {
+        throw err;
+      },
+    );
+  }
+
+  void onError(void Function(dynamic error) cb) {
+    when(
+      data: (_) {},
+      error: (err) => cb(err),
+    );
+  }
+}
