bd730e69614882cb108b3cd383873dcf804f9ed1
herveGuigoz <herve.guigoz@gmail.com>
2020-12-11 11:04:13 +0100

    👌 fix: add comment to document code on provider

--- lib/providers.dart
+++ lib/providers.dart
@@ -8,20 +8,25 @@ import 'models/path.dart';
 import 'network/repository.dart';
 import 'output/definition_printer.dart';
 
+/// OpenApi documentation url.
 final urlProvider = Provider<String>(null);
 
+/// Output directory for generated files.
 final directoryProvider = Provider((_) => 'build');
 
+/// Parent file name where all `Part '...';` will be injected.
 final mainFileNameProvider = Provider<String>((ref) {
   return 'output.dart';
 });
 
+/// Network call to load openApi documetation.
 final specsProvider = FutureProvider<String>((ref) async {
   final url = ref.read(urlProvider);
   stdout.writeln('Loading spec from $url..');
   return Repository.loadJson(url);
 });
 
+/// All Definition list in openApi documentation.
 final definitionsProvider = Provider<List<Definition>>((ref) {
   final definitions = <Definition>[];
   ref.read(specsProvider).whenData((value) {
@@ -36,10 +41,12 @@ final definitionsProvider = Provider<List<Definition>>((ref) {
   return definitions;
 });
 
+/// Find given Definition in openApi documentation.
 final definitionProvider = Provider.family<Definition, String>((ref, param) {
   return ref.read(definitionsProvider).firstWhere((def) => def.name == param);
 });
 
+/// All Paths list in openApi documentation.
 final pathsProvider = Provider<List<Path>>((ref) {
   final paths = <Path>[];
   ref.read(specsProvider).whenData((value) {
@@ -53,10 +60,15 @@ final pathsProvider = Provider<List<Path>>((ref) {
   return paths;
 });
 
+/// Find given Path in openApi documentation.
 final pathProvider = Provider.family<Path, String>((ref, param) {
-  return ref.read(pathsProvider).firstWhere((path) => path.iri == param);
+  return ref.read(pathsProvider).firstWhere(
+        (path) => path.iri == param,
+        orElse: () => null,
+      );
 });
 
+/// Translate openApi Property type to dart as String.
 final propertyTypeToString = Provider.family<String, Property>((ref, param) {
   if (!param.hasDefinition) {
     return param.type == Array ? 'List<${param.subType}>' : '${param.type}';
@@ -70,8 +82,10 @@ final propertyTypeToString = Provider.family<String, Property>((ref, param) {
   return 'List<${def.entity}>';
 });
 
+/// Where all `part '...';` will be saved.
 final stringBufferProvider = Provider((_) => StringBuffer());
 
+/// Generate file for given Definition.
 final writeToFile = FutureProvider.autoDispose.family<void, Definition>(
   (ref, def) async => DefinitionPrinter(ref.read, def).generate(),
 );
