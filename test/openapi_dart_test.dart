875b8de0a415f905c53cc97b115fff660523f7af
herveGuigoz <herve.guigoz@gmail.com>
2020-12-12 15:29:04 +0100

    ✅ test: parse definitions

--- test/openapi_dart_test.dart
+++ test/openapi_dart_test.dart
@@ -1,8 +1,40 @@
-// import 'package:openapi_dart/main.dart';
-// import 'package:test/test.dart';
+import 'dart:io';
 
-// void main() {
-//   test('calculate', () {
-//     expect(calculate(), 42);
-//   });
-// }
+import 'package:openapi_dart/models/definition.dart';
+import 'package:openapi_dart/providers.dart';
+import 'package:riverpod/all.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  final file = File('test/swagger.json');
+  final json = await file.readAsString();
+
+  test('parse definitions', () async {
+    final container = ProviderContainer(
+      overrides: [specsProvider.overrideWithValue(AsyncData(json))],
+    );
+    final definitions = container.read(definitionsProvider);
+    expect(definitions.length, 6);
+    expect(definitions.byName('ApiResponse').properties.length, 3);
+    expect(definitions.byName('Category').properties.length, 2);
+    expect(definitions.byName('Pet').properties.length, 6);
+    expect(definitions.byName('Pet').requiredProperties.length, 2);
+    expect(definitions.byName('Tag').properties.length, 2);
+    expect(definitions.byName('Order').properties.length, 6);
+    expect(definitions.byName('User').properties.length, 8);
+    expect(definitions.byName(''), null);
+  });
+
+  // test('parse paths', () async {
+  //   final container = ProviderContainer(
+  //     overrides: [specsProvider.overrideWithValue(AsyncData(json))],
+  //   );
+  //   final paths = container.read(pathsProvider);
+
+  //   for (final path in paths) {
+  //     print(path.iri);
+  //   }
+
+  //   expect(paths.length, 14);
+  // });
+}
